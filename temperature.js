f559d592e5a1ec5d95780137bfadd301605c3d08
Jonathan Expósito <alu0100696455@ull.edu.es>
2016-02-24 09:57:00 +0000

    Mejorada expresión regular

--- temperature.js
+++ temperature.js
@@ -2,17 +2,18 @@
 function calculate() {
   var result;
   var temp = original.value;
-  var regexp = /([-+]?\d+(?:\.\d*)?)\s*([fFcC])/;
-  
+  var regexp = /^\s*([-+]?\d+(?:\.\d*)?\s*(?:e\s*[-+]?\d+)?\s*)\º?([fc])[a-z]*$/i;
+
   var m = temp.match(regexp);
-  
+
   if (m) {
     var num = m[1];
     var type = m[2];
+    num = num.replace(/\s/g, '');
     num = parseFloat(num);
-    if (type == 'c' || type == 'C') {
+    if (type.match(/^c/i)) {
       result = (num * 9/5)+32;
-      result = result.toFixed(1)+" Farenheit"
+      result = result.toFixed(1)+" Fahrenheit"
     }
     else {
       result = (num - 32)*5/9;
@@ -21,6 +22,6 @@ function calculate() {
     converted.innerHTML = result;
   }
   else {
-    converted.innerHTML = "ERROR! Try something like '-4.2C' instead";
+    converted.innerHTML = "¡ERROR! Ha insertado una temperatura incorrecta. Ejemplo válido: 32.3e5 Fahrenheit";
   }
 }
