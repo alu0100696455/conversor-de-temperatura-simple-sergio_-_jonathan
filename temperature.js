26dd05da0e271203539c80dd1446823b6d6ec4a7
Jonathan Expósito <alu0100696455@ull.edu.es>
2016-02-24 13:27:58 +0000

    Expresión regular mejorada
    Ahora se comprueba todo en la expresión regular.

--- temperature.js
+++ temperature.js
@@ -1,15 +1,15 @@
 "use strict"; // Use ECMAScript 5 strict mode in browsers that support it
 function calculate() {
   var result;
-  var regexp = /^\s*([-+]?\d+(?:\.\d*)?\s*(?:e\s*[-+]?\d+)?\s*)\º?([a-z]*)/i;
+  var regexp = /^\s*([-+]?\d+(?:\.\d*)?\s*(?:e\s*[-+]?\d+)?\s*)\º?((f(a(h(r(e(n(h(e(i(t)?)?)?)?)?)?)?)?)?)|(c(e(l(s(i(u(s)?)?)?)?)?)?))$/i;
   var temp = original.value;
 
   var m = temp.match(regexp);
-  var unidades = ["fahrenheit", "celsius"];
-  var pref_f = unidades[0].match(m[2]);
-  var pref_c = unidades[1].match(m[2]);
+  //var unidades = ["fahrenheit", "celsius"];
+  //var pref_f = unidades[0].match(m[2]);
+  //var pref_c = unidades[1].match(m[2]);
 
-  if (m && (pref_f || pref_c)) {
+  if (m) {
     var num = m[1];
     var type = m[2];
     num = num.replace(/\s/g, '');
